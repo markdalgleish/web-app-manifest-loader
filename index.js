37d20bd8ab176c2150bf2cb6b5bef4e23fa8fe0c
Luiz Bucci <luizbucci@gmail.com>
2020-05-09 22:02:57 -0700

    fix to be compatible with ng9

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ function resolveImageSrc(loaderContext, image, callback) {
 
       // Update the image src property to match the generated filename
       // Is it always the first key in the assets object?
-      image.src = Object.keys(module.assets)[0];
+      if(module.assets)
+        image.src = Object.keys(module.assets)[0];
 
       callback(null);
     });
